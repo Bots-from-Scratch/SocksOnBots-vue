6790a1dd1664625e3af50842ec52c9f5f2799a20
Stefan <stefn0504@web.de>
2023-06-12 23:06:51 +0200

    WIP emit from client not working

---
+++ src/components/ConnectionState.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>State: {{ connected }}</p>
+  <p>State: {{ fooEvents }}</p>
+</template>
+
+<script>
+import { state } from "@/socket";
+
+export default {
+  name: "ConnectionState",
+
+  computed: {
+    connected() {
+      return state.connected;
+    },
+    fooEvents() {
+      return state.fooEvents;
+    }
+  }
+}
+</script>
+
