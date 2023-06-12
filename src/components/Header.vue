f5c0709a0c86e42257b5b8f627905fd1765efcda
Stefan <stefn0504@web.de>
2023-06-12 19:17:19 +0200

    Fix blockly z axis bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,19 @@
 </script>
 
 <template>
-  <header class="sticky z-50 top-0 bg-[#BA691D] w-full h-12 flex flex-row justify-around items-center">
-    <div></div>
+  <header class="sticky z-[100] top-0 bg-[#BA691D] w-full">
+    <div class="h-12 flex flex-row justify-around items-center">
+    <div>Logo?</div>
     <div class="flex flex-row justify-around gap-x-32 font-pixel text-white">
     <router-link to="/">Home</router-link>
     <router-link to="/game">Game</router-link>
     <router-link to="/about">About</router-link>
     </div>
     <div class="text-white font-pixel">Login</div>
-
+    </div>
+  <div class="sticky z-[100] top-12 w-full h-2 bg-[#D7873C]"></div>
+  <div class="sticky z-[100] top-14 w-full h-2 bg-[#EFA866]"></div>
   </header>
-  <div class="sticky z-50 top-12 w-full h-2 bg-[#D7873C]"></div>
-  <div class="sticky z-50 top-14 w-full h-2 bg-[#EFA866]"></div>
-
 </template>
 
 <style scoped>
