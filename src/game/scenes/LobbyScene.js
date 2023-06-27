ee6b43f67aa9681982ed5137233d7aa78e1b6a8c
Nathan Gebremichael <nathan.gebremichael@mediendesign.de>
2023-06-27 10:21:51 +0200

    Räume werden aufgelistet und haben eigenen Kontext

--- src/game/scenes/LobbyScene.js
+++ src/game/scenes/LobbyScene.js
@@ -1,6 +1,6 @@
 import { Scene } from "phaser";
 import { state } from "@/socket"
-import {GameScene} from "@/components/Game.vue";
+import { GameScene } from "@/components/Game.vue";
 
 
 class LobbyScene extends Scene {
@@ -18,7 +18,9 @@ class LobbyScene extends Scene {
         this.titleLobby = this.add.text(100, 20, 'LOBBY AUSWÄHLEN');
 
         localRooms.map((room, i) => {
-            this.add.text(100, 100 + (i*20), room).setInteractive().on('pointerover', (btn) => btn.add.setStyle({ fill: '#006db2' })).on('pointerout', (btn) => btn.setStyle({ fill: '#fff' })).on('pointerdown', () => this.scene.start('GameScene'));
+            let roomButtonEntry = this.add.text(100, 100 + (i * 30), room).setInteractive().on('pointerover', () => roomButtonEntry.setStyle({ fill: '#006db2' })).on('pointerout', () => roomButtonEntry.setStyle({ fill: '#fff' })).on('pointerdown', () => {
+                this.scene.start('GameScene')
+            });
             // this.playButton_lvl_1.setInteractive()
             // this.playButton_lvl_1.on('pointerover', () => this.playButton_lvl_1.setStyle({ fill: '#006db2' })).on('pointerout', () => this.playButton_lvl_1.setStyle({ fill: '#fff' })).on('pointerdown', () => this.scene.start('GameScene_Level_1'));
 
@@ -32,7 +34,7 @@ class LobbyScene extends Scene {
     generateRooms(size) {
         let temp = [];
         for (let i = 0; i < size; i++) {
-            temp.push("Room" + (i+1));
+            temp.push("Room" + (i + 1));
         }
         return temp;
     }
