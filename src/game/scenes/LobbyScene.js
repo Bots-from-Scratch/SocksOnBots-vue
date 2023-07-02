7273a27344fb8995807fc4802a06aa57652b8fd1
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2023-07-02 12:38:14 +0200

    Schriftart hinzugefügt

--- src/game/scenes/LobbyScene.js
+++ src/game/scenes/LobbyScene.js
@@ -1,53 +1,53 @@
 import { Scene } from "phaser";
-import { state, socket } from "@/socket"
+import { state, socket } from "@/socket";
 import { connectRoom } from "../../socket";
-
+import { textStyle } from "../utils";
 
 class LobbyScene extends Scene {
-    constructor() {
-        super('LobbyScene');
-    }
-    preload() {
+  constructor() {
+    super("LobbyScene");
+  }
+  preload() {}
 
-    }
+  create() {
+    const numberOfRooms = 10;
+    const localRooms = this.generateRooms(numberOfRooms);
 
-    create() {
-        const numberOfRooms = 10;
-        const localRooms = this.generateRooms(numberOfRooms);
+    this.titleLobby = this.add.text(100, 20, "LOBBY AUSWÄHLEN", textStyle);
 
-        this.titleLobby = this.add.text(100, 20, 'LOBBY AUSWÄHLEN');
+    localRooms.map((room, i) => {
+      let roomButtonEntry = this.add
+        .text(100, 100 + i * 30, room, textStyle)
+        .setInteractive()
+        .on("pointerover", () => {
+          roomButtonEntry.setStyle({ fill: "#006db2" });
+          // console.log(room);
+        })
+        .on("pointerout", () => roomButtonEntry.setStyle({ fill: "#fff" }))
+        .on("pointerdown", () => {
+          state.roomID = room;
 
-        localRooms.map((room, i) => {
-            let roomButtonEntry = this.add.text(100, 100 + (i * 30), room).setInteractive().on('pointerover', () => { 
-                roomButtonEntry.setStyle({ fill: '#006db2' }) 
-                console.log(room);
-            }).on('pointerout', () => roomButtonEntry.setStyle({ fill: '#fff' })).on('pointerdown', () => {
-                state.roomID = room;
-                
-                // AUS ConnectionManager - Textzwecke - evtl. auslagern
-                connectRoom(room);
+          // AUS ConnectionManager - Textzwecke - evtl. auslagern
+          connectRoom(room);
 
-
-                this.scene.start('GameScene');
-            });
-            // this.playButton_lvl_1.setInteractive()
-            // this.playButton_lvl_1.on('pointerover', () => this.playButton_lvl_1.setStyle({ fill: '#006db2' })).on('pointerout', () => this.playButton_lvl_1.setStyle({ fill: '#fff' })).on('pointerdown', () => this.scene.start('GameScene_Level_1'));
-
+          this.scene.start("GameScene");
         });
-    }
+      // this.playButton_lvl_1.setInteractive()
+      // this.playButton_lvl_1.on('pointerover', () => this.playButton_lvl_1.setStyle({ fill: '#006db2' })).on('pointerout', () => this.playButton_lvl_1.setStyle({ fill: '#fff' })).on('pointerdown', () => this.scene.start('GameScene_Level_1'));
+    });
+  }
 
-    // update() {
-    //     console.log(state.rooms);
-    // }
+  // update() {
+  //     console.log(state.rooms);
+  // }
 
-    generateRooms(size) {
-        let temp = [];
-        for (let i = 0; i < size; i++) {
-            temp.push("Room" + (i + 1));
-        }
-        return temp;
+  generateRooms(size) {
+    let temp = [];
+    for (let i = 0; i < size; i++) {
+      temp.push("Room" + (i + 1));
     }
+    return temp;
+  }
 }
 
-
-export default LobbyScene
+export default LobbyScene;
