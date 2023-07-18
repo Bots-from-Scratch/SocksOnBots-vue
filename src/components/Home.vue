f6b8f177f4ef789e24ed175b4b13f087e6ae8f1d
JDenu <jegatheeswarade84077>
2023-07-18 23:39:55 +0200

    Home page: new icon, new video, pixel-border

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,10 +15,22 @@ const cards = [
     <HeroBanner />
     <div class="container mx-auto flex justify-center gap-20 my-32 flex-wrap">
       <div class="w-full md:w-2/3 lg:w-1/2">
-        <video src="@/assets/video/Gesamt_hochkant.mp4" controls class="w-full"></video>
+        <video src="@/assets/video/Socks_on_Bots_Pitchvideo.mp4" controls class="w-full pixel-border-16"></video>
       </div>
+    </div>
+    <div class="flex items-center justify-end space-x-2 p-4">
+      <a href="https://www.instagram.com/socks_on_bots/" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/icon/instagram-icon.png" alt="Instagram" class="h-8" />
+      </a>
+      <a href="https://github.com/Bots-from-Scratch" target="_blank" rel="noopener noreferrer">
+        <img src="/src/assets/icon/github-icon.png" alt="GitHub" class="h-8" />
+      </a>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pixel-border-16 {
+  box-shadow: -16px 0 0 0 black, 16px 0 0 0 black, 0 -16px 0 0 black, 0 16px 0 0 black;
+}
+</style>
